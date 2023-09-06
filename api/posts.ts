53c748dc978e1e941b067440b66080c59e4c5411
Dylan Mooney <105092927+dylanmooney@users.noreply.github.com>
2023-09-06 17:58:06 +0100

    Remove empty config from fetch

--- api/posts.ts
+++ api/posts.ts
@@ -3,5 +3,5 @@ import { Post } from "@/types";
 import { API } from "@/constants";
 
 export async function getPosts(): Promise<Post[]> {
-  return await fetch(API.POSTS.GET(), {}).then((res) => res.json());
+  return await fetch(API.POSTS.GET()).then((res) => res.json());
 }
