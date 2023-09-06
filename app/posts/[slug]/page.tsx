4d33528dd082475f41e4f210635db27bda01f8e3
Dylan Mooney <105092927+dylanmooney@users.noreply.github.com>
2023-09-06 18:00:28 +0100

    Remove static generation

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,15 +1,5 @@
 import { getPosts } from "@/api/posts";
 
-// Rebuild the page every 1 hour.
-export const revalidate = 3600;
-
-// Static generation of each post page.
-export async function generateStaticParams() {
-  const posts = await getPosts();
-
-  return posts.map((post) => ({ slug: post.slug }));
-}
-
 type PostPageProps = {
   params: { slug: string };
 };
