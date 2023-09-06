a9993c91c57ebb4962b9e92541cf76d5da56050b
Dylan Mooney <105092927+dylanmooney@users.noreply.github.com>
2023-09-06 17:51:11 +0100

    Update base url to absolute

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,7 @@
-const BASE_URL = process.env.VERCEL_URL ?? "http://localhost:3000";
+const BASE_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 const BASE_API_URL = `${BASE_URL}/api`;
 
 export const API = {
